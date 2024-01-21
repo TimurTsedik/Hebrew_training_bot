96d2743c20f00c45ec9598cc7e45e3e10d7f3018
timur555 <timur.tsedik@gmail.com>
2024-01-21 18:44:45 +0200

    updated2 dict_jobs.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ def show_stats(message):
 def message_reply(message):
     sucsess = False
     text = message.text
-    print('request text: ', text)
+    print('request from user: ', message.from_user.first_name)
     markup = types.ReplyKeyboardMarkup(row_width=2)
     if len(userStep) == 0 or userStep.get(message.from_user.id) == 0 or userStep.get(message.from_user.id) == 5:
         if len(userStep) == 0:
